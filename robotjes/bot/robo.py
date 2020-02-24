8591a6c0b75f3fb3de4c5f6f68d346572cc4a847
Jan van Oorschot <janvanoorschot@gmail.com>
2020-02-24 09:40:23 +0100

    started remote communication work.

--- robotjes/bot/robo.py
+++ robotjes/bot/robo.py
@@ -6,69 +6,94 @@ class Robo(object):
         self.requestor = requestor
         self.id = uuid.uuid4()
 
+    def handle_result(self, result):
+        return result
+
     def forward(self, steps=1):
         result = self.requestor.execute([self.id, 'forward', steps])
+        return self.handle_result(result)
 
     def backward(self, steps=1):
-        pass
+        result = self.requestor.execute([self.id, 'backward', steps])
+        return self.handle_result(result)
 
     def left(self, steps=1):
-        pass
+        result = self.requestor.execute([self.id, 'left', steps])
+        return self.handle_result(result)
 
     def right(self, steps=1):
-        pass
+        result = self.requestor.execute([self.id, 'right', steps])
+        return self.handle_result(result)
 
     def pickUp(self):
-        pass
+        result = self.requestor.execute([self.id, 'pickUp'])
+        return self.handle_result(result)
 
     def putDown(self):
-        pass
+        result = self.requestor.execute([self.id, 'putDown'])
+        return self.handle_result(result)
 
     def eatUp(self):
-        pass
+        result = self.requestor.execute([self.id, 'eatUp'])
+        return self.handle_result(result)
 
     def paintWhite(self):
-        pass
+        result = self.requestor.execute([self.id, 'paintWhite'])
+        return self.handle_result(result)
 
     def paintBlack(self):
-        pass
+        result = self.requestor.execute([self.id, 'paintBlack'])
+        return self.handle_result(result)
 
     def stopPainting(self):
-        pass
+        result = self.requestor.execute([self.id, 'stopPainting'])
+        return self.handle_result(result)
 
     def leftIsClear(self):
-        pass
+        result = self.requestor.execute([self.id, 'leftIsClear'])
+        return self.handle_result(result)
 
     def leftIsBeacon(self):
-        pass
+        result = self.requestor.execute([self.id, 'leftIsBeacon'])
+        return self.handle_result(result)
 
     def leftIsWhite(self):
-        pass
+        result = self.requestor.execute([self.id, 'leftIsWhite'])
+        return self.handle_result(result)
 
     def leftIsBlack(self):
-        pass
+        result = self.requestor.execute([self.id, 'leftIsBlack'])
+        return self.handle_result(result)
 
     def frontIsClear(self):
-        pass
+        result = self.requestor.execute([self.id, 'frontIsClear'])
+        return self.handle_result(result)
 
     def frontIsBeacon(self):
-        pass
+        result = self.requestor.execute([self.id, 'frontIsBeacon'])
+        return self.handle_result(result)
 
     def frontIsWhite(self):
-        pass
+        result = self.requestor.execute([self.id, 'frontIsWhite'])
+        return self.handle_result(result)
 
     def frontIsBlack(self):
-        pass
+        result = self.requestor.execute([self.id, 'frontIsBlack'])
+        return self.handle_result(result)
 
     def rightIsClear(self):
-        pass
+        result = self.requestor.execute([self.id, 'rightIsClear'])
+        return self.handle_result(result)
 
     def rightIsBeacon(self):
-        pass
+        result = self.requestor.execute([self.id, 'rightIsBeacon'])
+        return self.handle_result(result)
 
     def rightIsWhite(self):
-        pass
+        result = self.requestor.execute([self.id, 'rightIsWhite'])
+        return self.handle_result(result)
 
     def rightIsBlack(self):
-        pass
+        result = self.requestor.execute([self.id, 'rightIsBlack'])
+        return self.handle_result(result)
 
--- robotjes/sim/engine.py
+++ robotjes/sim/engine.py
@@ -1,11 +1,77 @@
+LEGAL_COMMANDS = ["forward", "backward", "left", "right", "pickup", "putDown",
+                  "eatUp", "paintwhite", "paintBlack", "stopPainting",
+                  "leftIsClear", "leftisBeacon", "leftIsWhite", "leftIsBlack",
+                  "frontIsClear", "frontisBeacon", "frontIsWhite", "frontIsBlack",
+                  "rightIsClear", "rightisBeacon", "rightIsWhite", "rightIsBlack",
+                  ]
 class Engine(object):
 
     def __init__(self, map_file):
         self.map_file = map_file
 
-    def execute(self, cmd):
-        cmd.append("this is a response")
-        return cmd
-
     def get_recording(self):
         return None
+
+    def clean_cmd(self, cmd):
+        if not cmd or not type(cmd)==list or len(cmd)< 2:
+            return ["illegal"]
+        [id, command, *args] = cmd
+        if command not in LEGAL_COMMANDS:
+            return ["unknown"]
+        else:
+            return [command, *args]
+
+    def prepare_reply(self, cmd, *args):
+        print(f"!!!!!{cmd}")
+        return [cmd]
+
+    def execute(self, cmd):
+        [command, *args] = self.clean_cmd(cmd)
+        if command == "forward":
+            reply = ["forward_reply"]
+        elif command == "backward":
+            reply = ["backward_reply"]
+        elif command == "left":
+            reply = ["left_reply"]
+        elif command == "right":
+            reply = ["right_reply"]
+        elif command == "pickup":
+            reply = ["pickup_reply"]
+        elif command == "putDown":
+            reply = ["putDown_reply"]
+        elif command == "eatUp":
+            reply = ["eatUp_reply"]
+        elif command == "paintwhite":
+            reply = ["paintwhite_reply"]
+        elif command == "paintBlack":
+            reply = ["paintBlack_reply"]
+        elif command == "stopPainting":
+            reply = ["stopPainting_reply"]
+        elif command == "leftIsClear":
+            reply = ["leftIsClear_reply"]
+        elif command == "leftisBeacon":
+            reply = ["leftisBeacon_reply"]
+        elif command == "leftIsWhite":
+            reply = ["leftIsWhite_reply"]
+        elif command == "leftIsBlack":
+            reply = ["leftIsBlack_reply"]
+        elif command == "frontIsClear":
+            reply = ["frontIsClear_reply"]
+        elif command == "frontisBeacon":
+            reply = ["frontisBeacon_reply"]
+        elif command == "frontIsWhite":
+            reply = ["frontIsWhite_reply"]
+        elif command == "frontIsBlack":
+            reply = ["frontIsBlack_reply"]
+        elif command == "rightIsClear":
+            reply = ["rightIsClear_reply"]
+        elif command == "rightisBeacon":
+            reply = ["rightisBeacon_reply"]
+        elif command == "rightIsWhite":
+            reply = ["rightIsWhite_reply"]
+        elif command == "rightIsBlack":
+            reply = ["rightIsBlack_reply"]
+        else:
+            reply = ["error"]
+        return self.prepare_reply(reply)
+
