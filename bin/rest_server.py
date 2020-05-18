4e193382bd5b80db7db768bbee3660651cd4d99c
Jan van Oorschot <janvanoorschot@gmail.com>
2020-05-18 11:27:22 +0200

    Ongoing work on monitoring.

--- bin/rest_server.py
+++ bin/rest_server.py
@@ -3,18 +3,28 @@
 import sys, os
 import argparse
 import pika, json
-
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
 
+# get commandline arguments
 parser = argparse.ArgumentParser(description='Start the Bubbles/Robo REST Server.')
 parser.add_argument('--host', type=str, default='0.0.0.0', help='host')
 parser.add_argument('--port', type=int, default=8000, help='port')
 args = parser.parse_args()
 
-
-import uvicorn
-from roborest import app
+# initialise the roborest module
+import fastapi
+import roborest
+from roborest.bubble_rpc_client import BubbleRPCClient
+PIKA_URL = 'amqp://guest:guest@localhost:5672/%2F'
+BUBBLE_QUEUE = "bubble_queue"
+roborest.bubble_rpc_client = BubbleRPCClient(PIKA_URL, BUBBLE_QUEUE)
+roborest.app = fastapi.FastAPI()
+import roborest.bubble_rest
 from fastapi.staticfiles import StaticFiles
+roborest.app.mount("/", StaticFiles(directory="www"), name="www")
 
-app.mount("/", StaticFiles(directory="www"), name="www")
-uvicorn.run(app, host=args.host, port=args.port)
+# initialise the monitor module
+
+# start the webserver
+import uvicorn
+uvicorn.run(roborest.app, host=args.host, port=args.port)
--- roborest/__init__.py
+++ roborest/__init__.py
@@ -1,11 +1,6 @@
-from fastapi import FastAPI
-app = FastAPI()
+app = None
+bubble_rpc_client = None
 
-from .bubble_rpc_client import BubbleRPCClient
-PIKA_URL = 'amqp://guest:guest@localhost:5672/%2F'
-BUBBLE_QUEUE = "bubble_queue"
-bubble_rpc_client = BubbleRPCClient(PIKA_URL, BUBBLE_QUEUE)
-
-import roborest.bubble_rest
+# import roborest.bubble_rest
 
 
