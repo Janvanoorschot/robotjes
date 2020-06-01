16e881fda18d577d639d6794b1acb689eea708e4
Jan van Oorschot <janvanoorschot@gmail.com>
2020-06-01 13:41:27 +0200

    Work on game queueing protocol.

--- bubblehub/bubble.py
+++ bubblehub/bubble.py
@@ -1,10 +1,44 @@
-from bubblehub.model import BubbleSpec, ConnectionSpec, BubbleStatus
-from monitor import wrap_monitor
+import json
+import config
+
 
 class Bubble:
 
-    def __init__(self):
-        pass
+    def __init__(self, bubble_id, channel):
+        self.bubble_id = bubble_id
+        self.channel = channel
+        # create the incoming game exchange/queue
+        self.channel.exchange_declare(exchange=config.GAME_IN_EXCHANGE, exchange_type="topic")
+        result = self.channel.queue_declare('', exclusive=True)
+        self.queue_in_name = result.method.queue
+        self.channel.basic_consume(queue=self.queue_in_name, on_message_callback=self.on_game_message, auto_ack=True)
+        self.game_id = 'unknown'
+        # create the outgoing exchange
+        self.channel.exchange_declare(exchange=config.GAME_OUT_EXCHANGE, exchange_type="topic")
 
-    def start_game(self):
-        pass
+    def start_game(self, game_id):
+        self.game_id = game_id
+        self.channel.queue_bind(
+            exchange=config.GAME_IN_EXCHANGE,
+            queue=self.queue_in_name,
+            routing_key=self.game_id
+        )
+        reply = {
+            'cmd': "starting",
+            'bubble': self.bubble_id,
+            'game': self.game_id
+        }
+        j = json.dumps(reply)
+        self.channel.basic_publish(exchange=config.GAME_OUT_EXCHANGE,
+                                   routing_key=config.GAME_STATUS_QUEUE,
+                                   body=j)
+
+    def stop_game(self):
+        self.channel.queue_unbind(
+            exchange=config.GAME_IN_EXCHANGE,
+            queue=self.queue_in_name,
+            routing_key=self.game_id
+        )
+
+    def on_game_message(self, ch, method, props, body):
+        print(f"game message: {body}")
---
+++ config.py
@@ -0,0 +1,4 @@
+GAME_IN_EXCHANGE = 'game_in'
+GAME_OUT_EXCHANGE = 'game_out'
+
+GAME_STATUS_QUEUE = 'status'
