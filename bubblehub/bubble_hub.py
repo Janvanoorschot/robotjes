e56983a269da425c14bdc237119557f57678dc4e
Jan van Oorschot <janvanoorschot@gmail.com>
2020-06-13 13:36:53 +0200

    move config to config.py.

--- bubblehub/bubble_hub.py
+++ bubblehub/bubble_hub.py
@@ -1,7 +1,5 @@
 import json
-from bubblehub.model import BubbleSpec, ConnectionSpec, BubbleStatus
-from monitor import wrap_monitor
-from .rcp_client import RPCClient
+from bubblehub.model import BubbleSpec
 
 class BubbleHub:
 
@@ -10,7 +8,6 @@ class BubbleHub:
         self.exchange = exchange
         self.queue = queue
 
-    # @wrap_monitor('create_bubble')
     def create_bubble(self, specs: BubbleSpec):
         body = json.dumps(specs.dict())
         self.channel.basic_publish(exchange=self.exchange,
--- config.py
+++ config.py
@@ -1,4 +1,17 @@
+PIKA_URL = 'amqp://guest:guest@localhost:5672/%2F'
+LOG_CONFIG_FILE = "bin/log.conf"
+
+MONITOR_EXCHANGE = "monitor_exchange"
+BUBBLES_EXCHANGE = "bubbles_exchange"
 GAME_IN_EXCHANGE = 'game_in'
 GAME_OUT_EXCHANGE = 'game_out'
 
+BUBBLEHUB_QUEUE = "bubblehub_queue"
+BUBBLES_QUEUE = "bubbles_queue"
 GAME_STATUS_QUEUE = 'status'
+
+DBASE_USER = "rmprod"
+DBASE_PWD = "rmprodsecret"
+DBASE_HOST = "localhost"
+DBASE_PORT = 5432
+DBASE_NAME = "monitor"
