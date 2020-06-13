c9dc4aafa929f88d1e6519d93e71aee89af15aac
Jan van Oorschot <janvanoorschot@gmail.com>
2020-06-13 15:24:57 +0200

    Ongoing

--- bubblehub/bubble_hub.py
+++ bubblehub/bubble_hub.py
@@ -1,16 +1,15 @@
 import json
 from bubblehub.model import BubbleSpec
+import config
 
 class BubbleHub:
 
-    def __init__(self, channel, exchange, queue):
+    def __init__(self, channel):
         self.channel = channel
-        self.exchange = exchange
-        self.queue = queue
 
     def create_bubble(self, specs: BubbleSpec):
         body = json.dumps(specs.dict())
-        self.channel.basic_publish(exchange=self.exchange,
-                             routing_key=self.queue,
+        self.channel.basic_publish(exchange=config.BUBBLES_EXCHANGE,
+                             routing_key=config.BUBBLES_QUEUE,
                              body=body)
 
