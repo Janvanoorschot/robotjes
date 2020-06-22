73254b60c61350cbd9ba9a8c9a55bdac85e3a63c
Jan van Oorschot <janvanoorschot@gmail.com>
2020-06-22 20:08:22 +0200

    Ongoing

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,3 +1,4 @@
 from .robo_admin import RoboAdminWindow, RoboAdmin
-from .robo_teacher import RoboTeacherWindow, RoboTeacher
+from .robo_views import RoboTeacherWindow
+from .robo_teacher import RoboTeacher
 from .robo_requestor import RoboRequestor
