6b849f8258dc81f33584c39a6afc97a030edeb3f
Jan van Oorschot <janvanoorschot@gmail.com>
2020-06-27 09:21:30 +0200

    Starting on Player UI and renaming Teacher to Umpire.

--- ui/robo_player_views.py
+++ ui/robo_player_views.py
@@ -1,4 +1,5 @@
 from gi.repository import Gtk, Gdk
+from .robo_gen_views import GamesComponent
 
 class RoboPlayerWindow(Gtk.Window):
 
@@ -12,8 +13,6 @@ class RoboPlayerWindow(Gtk.Window):
         toppane = Gtk.Paned.new(Gtk.Orientation.HORIZONTAL)
 
         leftbox = Gtk.Grid(expand=True, orientation=Gtk.Orientation.VERTICAL)
-        self.creategame_component = CreateGameComponent(self.model, self)
-        leftbox.add(self.creategame_component)
         self.games_component = GamesComponent(self.model, self)
         leftbox.add(self.games_component)
         rightbox = Gtk.Grid()
@@ -47,159 +46,6 @@ class RoboPlayerWindow(Gtk.Window):
         self.textfield.set_text(text)
 
     def refresh(self):
-        self.creategame_component.refresh()
         self.games_component.refresh()
 
 
-class CreateGameComponent(Gtk.Grid):
-
-    def __init__(self, model, owner):
-        super().__init__(expand=True, orientation=Gtk.Orientation.VERTICAL)
-        self.model = model
-        self.owner = owner
-        self.__construct()
-
-    def __construct(self):
-        self.mazes_component = MazesComponent(self.model, self.owner)
-        self.add(self.mazes_component)
-        self.add(Gtk.Label("name"))
-        self.name_field = Gtk.Entry()
-        self.add(self.name_field)
-        self.add(Gtk.Label("password"))
-        self.password_field = Gtk.Entry()
-        self.add(self.password_field)
-        create_button = Gtk.Button(label="Create")
-        create_button.connect("clicked", self.on_create_button_clicked)
-        self.add(create_button )
-        stop_button = Gtk.Button(label="Stop")
-        stop_button.connect("clicked", self.on_stop_button_clicked)
-        self.add(stop_button)
-
-    def on_create_button_clicked(self, button):
-        name = self.name_field.get_text()
-        password = self.name_field.get_text()
-        maze_id = self.mazes_component.get_selected_id()
-        self.owner.do_signal("EVT_CREATE_GAME", self, {
-            "name": name,
-            "password": password,
-            "maze_id": maze_id
-        })
-
-    def on_stop_button_clicked(self, button):
-        pass
-
-    def refresh(self):
-        self.mazes_component.refresh()
-
-
-class GamesComponent(Gtk.Grid):
-
-    def __init__(self, model, owner):
-        super().__init__(expand=True, orientation=Gtk.Orientation.VERTICAL)
-        self.model = model
-        self.owner = owner
-        self.games_field = None
-        self.games_model = None
-        self.games_selection = None
-        self.games_selected = None
-        self.__construct()
-
-    def __construct(self):
-        glabel = Gtk.EventBox()
-        glabel.add(Gtk.Label("Available Games"))
-        color = Gdk.color_parse('grey')
-        glabel.modify_bg(Gtk.StateType.NORMAL, color)
-        self.attach(glabel, 0, 0, 1, 1)
-
-        # list of the games
-        self.games_field = Gtk.TreeView(expand=True)
-        renderer = Gtk.CellRendererText()
-        column = Gtk.TreeViewColumn("Name", renderer, text=0)
-        self.games_field.append_column(column)
-        self.scrollable_gameslist = Gtk.ScrolledWindow()
-        self.scrollable_gameslist.set_vexpand(True)
-        self.scrollable_gameslist.add(self.games_field)
-        self.games_model = Gtk.ListStore(str, str)
-        self.games_field.set_model(self.games_model)
-        self.games_selection = self.games_field.get_selection()
-        self.games_selection.set_mode(Gtk.SelectionMode.SINGLE)
-        self.games_selection.connect("changed", self.selection_changed)
-        self.attach_next_to(self.scrollable_gameslist, glabel, Gtk.PositionType.BOTTOM, 1, 1)
-
-    def refresh(self):
-        def cb(games):
-            for game_id, game in games.items():
-                for entry in self.games_model:
-                    if entry[1] == game_id:
-                        break
-                else:
-                    self.games_model.append([game['id'], game_id])
-            for entry in self.games_model:
-                if not entry[1] in games:
-                    self.games_model.remove(entry.iter)
-        self.model.list_games(cb)
-
-    def selection_changed(self, tree_selection):
-        (model, pathlist) = tree_selection.get_selected_rows()
-        for path in pathlist :
-            tree_iter = model.get_iter(path)
-            self.games_selected = model.get_value(tree_iter,1)
-
-    def get_selected_id(self):
-        return self.games_selected
-
-
-class MazesComponent(Gtk.Grid):
-
-    def __init__(self, model, owner):
-        super().__init__(expand=True, orientation=Gtk.Orientation.VERTICAL)
-        self.model = model
-        self.owner = owner
-        self.mazes_field = None
-        self.mazes_model = None
-        self.mazes_selection = None
-        self.mazes_selected = None
-        self.__construct()
-
-    def __construct(self):
-        glabel = Gtk.EventBox()
-        glabel.add(Gtk.Label("Available Mazes"))
-        color = Gdk.color_parse('grey')
-        glabel.modify_bg(Gtk.StateType.NORMAL, color)
-        self.attach(glabel, 0, 0, 1, 1)
-
-        self.mazes_field = Gtk.TreeView(expand=True)
-        renderer = Gtk.CellRendererText()
-        column = Gtk.TreeViewColumn("Name", renderer, text=0)
-        self.mazes_field.append_column(column)
-        self.scrollable_mazeslist = Gtk.ScrolledWindow()
-        self.scrollable_mazeslist.set_vexpand(True)
-        self.scrollable_mazeslist.add(self.mazes_field)
-        self.mazes_model = Gtk.ListStore(str, str)
-        self.mazes_field.set_model(self.mazes_model)
-        self.mazes_selection = self.mazes_field.get_selection()
-        self.mazes_selection.set_mode(Gtk.SelectionMode.SINGLE)
-        self.mazes_selection.connect("changed", self.selection_changed)
-        self.attach_next_to(self.scrollable_mazeslist, glabel, Gtk.PositionType.BOTTOM, 1, 1)
-
-    def refresh(self):
-        def cb(mazes):
-            for maze_id, maze in mazes.items():
-                for entry in self.mazes_model:
-                    if entry[1] == maze_id:
-                        break
-                else:
-                    self.mazes_model.append([maze['name'], maze_id])
-            for entry in self.mazes_model:
-                if not entry[1] in mazes:
-                    self.mazes_model.remove(entry.iter)
-        self.model.list_mazes(cb)
-
-    def selection_changed(self, tree_selection):
-        (model, pathlist) = tree_selection.get_selected_rows()
-        for path in pathlist :
-            tree_iter = model.get_iter(path)
-            self.mazes_selected = model.get_value(tree_iter,1)
-
-    def get_selected_id(self):
-        return self.mazes_selected
--- ui/robo_umpire_views.py
+++ ui/robo_umpire_views.py
@@ -1,4 +1,5 @@
 from gi.repository import Gtk, Gdk
+from .robo_gen_views import GamesComponent
 
 class RoboUmpireWindow(Gtk.Window):
 
@@ -90,63 +91,6 @@ class CreateGameComponent(Gtk.Grid):
 
     def refresh(self):
         self.mazes_component.refresh()
-
-
-class GamesComponent(Gtk.Grid):
-
-    def __init__(self, model, owner):
-        super().__init__(expand=True, orientation=Gtk.Orientation.VERTICAL)
-        self.model = model
-        self.owner = owner
-        self.games_field = None
-        self.games_model = None
-        self.games_selection = None
-        self.games_selected = None
-        self.__construct()
-
-    def __construct(self):
-        glabel = Gtk.EventBox()
-        glabel.add(Gtk.Label("Available Games"))
-        color = Gdk.color_parse('grey')
-        glabel.modify_bg(Gtk.StateType.NORMAL, color)
-        self.attach(glabel, 0, 0, 1, 1)
-
-        # list of the games
-        self.games_field = Gtk.TreeView(expand=True)
-        renderer = Gtk.CellRendererText()
-        column = Gtk.TreeViewColumn("Name", renderer, text=0)
-        self.games_field.append_column(column)
-        self.scrollable_gameslist = Gtk.ScrolledWindow()
-        self.scrollable_gameslist.set_vexpand(True)
-        self.scrollable_gameslist.add(self.games_field)
-        self.games_model = Gtk.ListStore(str, str)
-        self.games_field.set_model(self.games_model)
-        self.games_selection = self.games_field.get_selection()
-        self.games_selection.set_mode(Gtk.SelectionMode.SINGLE)
-        self.games_selection.connect("changed", self.selection_changed)
-        self.attach_next_to(self.scrollable_gameslist, glabel, Gtk.PositionType.BOTTOM, 1, 1)
-
-    def refresh(self):
-        def cb(games):
-            for game_id, game in games.items():
-                for entry in self.games_model:
-                    if entry[1] == game_id:
-                        break
-                else:
-                    self.games_model.append([game['id'], game_id])
-            for entry in self.games_model:
-                if not entry[1] in games:
-                    self.games_model.remove(entry.iter)
-        self.model.list_games(cb)
-
-    def selection_changed(self, tree_selection):
-        (model, pathlist) = tree_selection.get_selected_rows()
-        for path in pathlist :
-            tree_iter = model.get_iter(path)
-            self.games_selected = model.get_value(tree_iter,1)
-
-    def get_selected_id(self):
-        return self.games_selected
 
 
 class MazesComponent(Gtk.Grid):
