26c907efbbc74c6aa666e100f592695273e4988a
Jan van Oorschot <janvanoorschot@gmail.com>
2020-09-25 22:41:48 +0200

    Working on CLI versions of umpire/player.

--- cli/cli_umpire.py
+++ cli/cli_umpire.py
@@ -7,6 +7,10 @@ class CLIUmpire():
         self.requestor = CLIRequestor(loop, url)
 
     async def run_game(self, umpire, name, password, maze):
+        list = await self.requestor.list_games()
+        for id, game_name in list.items():
+            if name == game_name:
+                raise Exception(f"game {game_name}/{id} already running")
         game_id = await self.requestor.create_game(umpire, name, password, maze)
         status = await self.requestor.status_game(game_id)
         return status
