ca0d3843d9b83771e42cfad1c2899c77b6b67fee
Jan van Oorschot <janvanoorschot@gmail.com>
2020-09-27 11:55:43 +0200

    Working on CLI versions of umpire/player.

--- cli/cli_umpire.py
+++ cli/cli_umpire.py
@@ -1,16 +1,39 @@
-import requests
+import collections
 from . import CLIRequestor
+import asyncio
+
 
 class CLIUmpire():
 
     def __init__(self, loop, url):
         self.requestor = CLIRequestor(loop, url)
+        self.game_id = None
+        self.ready = False
+        self.success = False
+        self.started = False
+        self.lock = asyncio.Lock()
 
     async def run_game(self, umpire, name, password, maze):
+        """ Validate the params, create the game and wait for the game to finish. """
+        await self.lock.acquire()
         list = await self.requestor.list_games()
         for id, game_name in list.items():
             if name == game_name:
                 raise Exception(f"game {game_name}/{id} already running")
-        game_id = await self.requestor.create_game(umpire, name, password, maze)
-        status = await self.requestor.status_game(game_id)
-        return status
+        self.game_id = await self.requestor.create_game(umpire, name, password, maze)
+        if not self.game_id:
+            raise Exception(f"create game failed")
+        await self.lock.acquire()
+        return self.game_id
+
+    async def timer(self):
+        if not self.ready:
+            status = await self.requestor.status_game(self.game_id)
+            if isinstance(status, collections.Mapping) and len(status) > 0:
+                self.started = True
+            if self.started:
+                isStarted = status['status']['isStarted']
+                isStopped = status['status']['isStopped']
+                isSuccess = status['status']['isSuccess']
+                print(f"{isStarted}/{isStopped}/{isSuccess}")
+                # self.lock.release()
