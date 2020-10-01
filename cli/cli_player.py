462f25fb4530e21b7eec32b5a1ebc113f37e5514
Jan van Oorschot <janvanoorschot@gmail.com>
2020-10-01 12:05:07 +0200

    Improved CLI state

--- cli/cli_player.py
+++ cli/cli_player.py
@@ -11,15 +11,17 @@ class CLIPlayer():
     https://docs.python.org/3/library/importlib.html#importlib.import_module
     """
 
-    def __init__(self, loop, url):
+    def __init__(self, loop, url, client):
         self.requestor = CLIRequestor(loop, url)
+        self.client = client
         self.player_id = str(uuid.uuid4())
         self.game_id = None
-        self.ready = False
+        self.game_tick = None
         self.success = False
         self.started = False
         self.stopped = False
         self.lock = asyncio.Lock()
+        self.players = {}
 
     async def run_game(self, player_name, game_name, password, module_name):
         """ Validate the params, join the game and wait for the game to finish. """
@@ -34,24 +36,54 @@ class CLIPlayer():
                 else:
                     raise Exception(f"invalid player module {module_name}")
                 success = await self.requestor.register_player(self.player_id, player_name, self.game_id, password)
-                if success:
-                    # the game has not yet started, that should come later
-                    pass
-                else:
+                if not success:
                     raise Exception(f"can not join game {game_name}")
+                else:
+                    self.callback('registered', self.player_id)
+                break
+        else:
+            raise Exception(f"no such game {game_name}")
         await self.lock.acquire()
         return self.success
 
     async def timer(self):
-        if not self.ready:
+        if not self.stopped:
             status = await self.requestor.status_game(self.game_id)
-            if self.started and isinstance(status, collections.Mapping) and len(status) <= 0:
-                self.lock.release()
-                return
-            if not self.started and isinstance(status, collections.Mapping) and len(status) > 0 and status['status']['isStarted']:
-                self.started = True
-            if not self.stopped and status['status']['isStopped']:
-                self.stopped = True
-                self.succes = status['status']['isSuccess']
-                self.lock.release()
-                return
+            if status and isinstance(status, collections.Mapping):
+                if 'players' in status:
+                    # check for new players
+                    for player in status['players']:
+                        player_name = player['player_name']
+                        player_id = player['player_id']
+                        if player_id not in self.players:
+                            self.players[player_id] = player_name
+                            self.callback('player', player_id, player_name)
+                if 'status' in status:
+                    if not self.stopped and status['status']['isStopped']:
+                        # normal stop
+                        self.stopped = True
+                        self.success = status['status']['isSuccess']
+                        self.callback('stopped', self.success)
+                        self.lock.release()
+                        return
+                    if not self.started and status['status']['isStarted']:
+                        # normal game start
+                        self.started = True
+                        self.stopped = False
+                        self.callback('started')
+                if 'tick' in status and status['tick'] != self.game_tick:
+                    self.game_tick = status['tick']
+                    self.callback('tick', self.game_tick)
+                if len(status) <= 0:
+                    # emergency stop
+                    self.stopped = True
+                    self.success = False
+                    self.callback('stopped', self.success)
+                    self.lock.release()
+                    return
+
+    def callback(self, cmd, *args):
+        invert_op = getattr(self.client, cmd, None)
+        if callable(invert_op):
+            invert_op(*args)
+
--- cli/cli_umpire.py
+++ cli/cli_umpire.py
@@ -5,15 +5,16 @@ import asyncio
 
 class CLIUmpire():
 
-    def __init__(self, loop, url):
+    def __init__(self, loop, url, client):
         self.requestor = CLIRequestor(loop, url)
+        self.client = client
         self.game_id = None
-        self.ready = False
+        self.game_tick = None
         self.success = False
         self.started = False
         self.stopped = False
         self.lock = asyncio.Lock()
-        self.players = set()
+        self.players = {}
 
     async def run_game(self, umpire, name, password, maze):
         """ Validate the params, create the game and wait for the game to finish. """
@@ -23,26 +24,49 @@ class CLIUmpire():
             if name == game_name:
                 raise Exception(f"game {game_name}/{id} already running")
         self.game_id = await self.requestor.create_game(umpire, name, password, maze)
+        self.callback("registered", self.game_id)
         if not self.game_id:
             raise Exception(f"create game failed")
         await self.lock.acquire()
         return self.success
 
     async def timer(self):
-        if not self.ready:
+        if not self.stopped:
             status = await self.requestor.status_game(self.game_id)
-            if self.started and isinstance(status, collections.Mapping) and len(status) <= 0:
-                self.lock.release()
-                return
-            if not self.started and isinstance(status, collections.Mapping) and len(status) > 0 and status['status']['isStarted']:
-                self.started = True
-            if not self.stopped and status['status']['isStopped']:
-                self.stopped = True
-                self.succes = status['status']['isSuccess']
-                self.lock.release()
-                return
-            if isinstance(status, collections.Mapping) and len(status) > 0:
-                for player in status['players']:
-                    if player not in self.players:
-                        print(f"new player: {player}")
-                        self.players.add(player)
+            if status and isinstance(status, collections.Mapping):
+                if 'players' in status:
+                    # check for new players
+                    for player in status['players']:
+                        player_name = player['player_name']
+                        player_id = player['player_id']
+                        if player_id not in self.players:
+                            self.players[player_id] = player_name
+                            self.callback('player', player_id, player_name)
+                if 'status' in status:
+                    if not self.stopped and status['status']['isStopped']:
+                        # normal stop
+                        self.stopped = True
+                        self.success = status['status']['isSuccess']
+                        self.callback('stopped', self.success)
+                        self.lock.release()
+                        return
+                    if not self.started and status['status']['isStarted']:
+                        # normal game start
+                        self.started = True
+                        self.stopped = False
+                        self.callback('started')
+                if 'tick' in status and status['tick'] != self.game_tick:
+                    self.game_tick = status['tick']
+                    self.callback('tick', self.game_tick)
+                if len(status) <= 0:
+                    # emergency stop
+                    self.stopped = True
+                    self.success = False
+                    self.callback('stopped', self.success)
+                    self.lock.release()
+                    return
+
+    def callback(self, cmd, *args):
+        invert_op = getattr(self.client, cmd, None)
+        if callable(invert_op):
+            invert_op(*args)
