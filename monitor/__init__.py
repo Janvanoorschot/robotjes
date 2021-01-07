e7d1b90182e06f9f743c38647b4a21d75703bb59
Jan van Oorschot <janvanoorschot@gmail.com>
2021-01-07 10:50:35 +0100

    Starting with tracelog.

--- monitor/__init__.py
+++ monitor/__init__.py
@@ -3,5 +3,6 @@ from . import monitor_client
 from .monitor_util import get_monitor, wrap_monitor
 from .monitor_server import MonitorServer
 from .monitor_logger import MonitorLogger
+from .trace_log import TraceLog
 
 mon=None
