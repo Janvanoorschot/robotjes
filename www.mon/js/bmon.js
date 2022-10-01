487af585033ec2f4a28f863686f04a3ce3c2fc3c
jan <janvanoorschot@gmail.com>
2022-10-01 20:39:22 +0200

    ongoing

--- www.mon/js/bmon.js
+++ www.mon/js/bmon.js
@@ -1,60 +1,57 @@
-(function($) {
+(function ($) {
 
     // create the robomind namespace 'rm'
     $.fn.bmon = {};
     $.fn.bmon.module = {};
 
-        var defaults = {
-            aap: 'noot'
-        };
+    var defaults = {
+        aap: 'noot'
+    };
 
-        $.fn.bmon.bubble_monitor =
-            function(node, modules) {
-                var that = {};
-                for (n in defaults) {
-                    that[n] = defaults[n];
-                }
-                that.node = node;
-                that.modules = modules;
-
-                // public API
-                that.start = function () {
-                    doStart(that);
-                };
-
-                that.timer = function () {
-                };
-
-                that.onmessage = function (data) {
-                };
-
-                populate(that)
-
-                return that;
-            };
-
-        function populate(that) {
-            Object.entries(that.modules).forEach(([mname,mtype]) => {
-                let classname = `.${mname}`;
-                that.node.find(classname).each(function(ix) {
-                    console.log($(this).attr('id'));
-                });
-
-            })
-        }
-
-        function doStart(that) {
-        }
-
-    $.fn.bmon.module.logmodule =
-        function() {
+    $.fn.bmon.bubble_monitor =
+        function (node, modules) {
             var that = {};
             for (n in defaults) {
                 that[n] = defaults[n];
             }
+            that.node = node;
+            that.moduledefs = modules;
+            that.modules = {};
+
+            that.timer = function () {
+            };
+
+            that.onmessage = function (data) {
+            };
+
+            populate(that)
+
+            return that;
+        };
+
+    function populate(that) {
+        // find modules to manage
+        Object.entries(that.moduledefs).forEach(([mname, mtype]) => {
+            let classname = `.${mname}`;
+            that.node.find(classname).each(function (ix) {
+                let node = $(this);
+                let args = node.attr("data-module");
+                console.log(args);
+                let module = mtype(node, args)
+                that.modules[node.attr('id')] = module;
+            });
+
+        })
+    }
+
+    $.fn.bmon.module.logmodule =
+        function (node, args) {
+            var that = {};
+            that.node = node;
+            that.args = args;
 
             // public API
-            that.start = function () {
+            that.apply = function (data) {
             };
 
             return that;
